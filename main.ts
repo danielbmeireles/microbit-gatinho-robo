5c879af0cf330e733b207a94a4446c799fd1e6bd
Daniel Meireles <danielbmeireles@users.noreply.github.com>
2021-02-21 11:49:31 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,66 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 4; index++) {
+        basic.showLeds(`
+            # # . . #
+            # # . . #
+            . # # # .
+            . # # # .
+            . # . # .
+            `)
+        basic.showLeds(`
+            # # . . .
+            # # . . #
+            . # # # #
+            . # # # .
+            . . . . .
+            `)
+    }
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # # # # #
+        . # # # .
+        . # # # .
+        `)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        # # . . #
+        # # . . #
+        . # # # .
+        # # # # #
+        # # # # #
+        `)
+    basic.showLeds(`
+        . # # . #
+        . # # . #
+        . . # # .
+        # . # # #
+        # # # # #
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    for (let index = 0; index < 4; index++) {
+        basic.showLeds(`
+            . # # # #
+            . # # # .
+            . # # # .
+            . . # . .
+            # # # # #
+            `)
+        basic.showLeds(`
+            # # # # .
+            . # # # .
+            . # # # .
+            . . # . .
+            # # # # #
+            `)
+    }
+})
+basic.showLeds(`
+    # # . # #
+    # . . . #
+    . # . # .
+    . . # . .
+    # . . . #
+    `)
